b74449276340b78463f15563d4fc6126b2be3f06
furkanevin <furkanevin00@gmail.com>
2023-09-05 23:10:36 +0300

    global test

---
+++ src/App.test.js
@@ -0,0 +1,27 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import App from './App';
+
+test('Uygulama doğru şekilde çalışıyor mu?', async () => {
+  render(<App />);
+
+  // gerekli bileşenler
+  const nameInp = screen.getByLabelText('İsim');
+  const mailInp = screen.getByLabelText('Email');
+  const button = screen.getByRole('button', {
+    name: 'Kullanıcı Ekle',
+  });
+
+  // formu doldur
+  user.type(nameInp, 'elif');
+  user.type(mailInp, 'elss@gmail.com');
+
+  // formu gönder
+  user.click(button);
+
+  // isim değerine karşılık gelen bir tablo hücresi ekran basıldı mı?
+  // state güncellenmesi sonucu ekrna basıldığınıdan
+  // async elemanları getiren find komutnu kullandık
+  await screen.findByRole('cell', { name: 'elif' });
+  await screen.findByRole('cell', { name: 'elss@gmail.com' });
+});
